cae18bf53c0c816ef1bf819dffc3ecb828d69c9a
GatchaAddict <borbakristel.workspace@gmail.com>
2025-02-18 13:28:41 +0800

    Add files via upload

---
+++ Grocery/grocerywindows/grocerywindows/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace grocerywindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.admin = new System.Windows.Forms.Button();
+            this.customer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // admin
+            // 
+            this.admin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.admin.AutoSize = true;
+            this.admin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(31)))), ((int)(((byte)(20)))));
+            this.admin.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.admin.Location = new System.Drawing.Point(457, 327);
+            this.admin.Name = "admin";
+            this.admin.Size = new System.Drawing.Size(151, 52);
+            this.admin.TabIndex = 0;
+            this.admin.Text = "ADMIN";
+            this.admin.UseVisualStyleBackColor = false;
+            this.admin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // customer
+            // 
+            this.customer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.customer.AutoSize = true;
+            this.customer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(31)))), ((int)(((byte)(20)))));
+            this.customer.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.customer.Location = new System.Drawing.Point(457, 406);
+            this.customer.Name = "customer";
+            this.customer.Size = new System.Drawing.Size(151, 52);
+            this.customer.TabIndex = 1;
+            this.customer.Text = "CUSTOMER";
+            this.customer.UseVisualStyleBackColor = false;
+            this.customer.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1064, 534);
+            this.Controls.Add(this.customer);
+            this.Controls.Add(this.admin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ardiente Capital";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button admin;
+        private System.Windows.Forms.Button customer;
+    }
+}
+
